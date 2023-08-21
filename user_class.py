42e11e2e0e87d24d18257a952e3c4eae8b753264
TetianaShevch <139060254+TetianaShevch@users.noreply.github.com>
2023-08-21 22:10:43 +0300

    Third Commit

--- user_class.py
+++ user_class.py
@@ -1,6 +1,6 @@
 from address_book_class import AddressBook
 
-class user():
+class User():
     def __init__(self, user_name:str, User_AddressBook:AddressBook):
         self.name = user_name
         self.User_AddressBook = User_AddressBook
